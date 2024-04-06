c9306a566cd43f67384b45e161d4b2ee890d00c3
Admin786t <148454084+Admin786t@users.noreply.github.com>
2024-04-07 00:46:12 +0500

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ import axios from 'axios'
 global.owner = [
   ['918360234087', 'WHITE', true], 
   ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
+  ['923259258115', '𝑺𝑯𝑨𝑯𝑰𝑫 𝑲𝑯𝑨𝑵 -(𝑴𝑫)', true], 
   ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
   ['233533763772', 'Abraham Dwamena2', true],
   ['233531910067', 'Abraham Dwamena3', true],
@@ -18,9 +18,9 @@ global.owner = [
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['923184070915', '94789481495', '923184474176','233531910067','233208179431'] 
-global.prems = ['923184070915','233208179431','94789481495', '918360234087','233531910067']
-global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087','233531910067']
+global.mods = ['923259258115', '94789481495', '923259258115','233531910067','233208179431'] 
+global.prems = ['923259258115','233208179431','94789481495', '918360234087','233531910067']
+global.allowed = ['923259258115', '233208179431', '94789481495', '918360234087','233531910067']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -54,22 +54,22 @@ global.APIKeys = { // APIKey Here
   'https://api-fgmods.ddns.net': 'fg-dylux'
 }
 
-// Sticker WM
-global.botname = 'XLICON-V2'
+// Sticker MD
+global.botname = 'Shahid-MD'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
-global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
-global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
-global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
-global.fgsc = 'https://github.com/salmanytofficial/XLICON-V2-MD' 
-global.fgyt = 'https://youtube.com/@s4salmanyt'
-global.fgpyp = 'https://youtube.com/@s4salmanyt'
-global.fglog = 'XLICON.jpg' 
-global.thumb = fs.readFileSync('./XLICON.jpg')
+global.packname = 'MR Drug TEAM' 
+global.author = '@Shahid-MD' 
+global.menuvid = 'https://telegra.ph/file/9bb79c87d32ee4f0cf3ec.jpg'
+global.igfg = '▢✓ Follow My channelhttps://whatsapp.com/channel/0029Vaa3Nz9FSAt1bxpPwt3x' 
+global.dygp = 'https://chat.whatsapp.com/BIhyo7byRNP4ZvUl5WH0dB'
+global.fgsc = 'https://github.com/Admin786T/SHAHID-MD' 
+global.fgyt = 'https://www.youtube.com/@MrRAJA7HACKER'
+global.fgpyp = 'https://www.youtube.com/@MrRAJA7HACKER'
+global.fglog = 'SHAHID-MD.jpg' 
+global.thumb = fs.readFileSync('./SHAHID-MD.jpg')
 
 
-global.wait = '*🕣 _XLICON IS LOADIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.wait = '*🕣 _SHAHID-MD IS LOADIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✔️'
